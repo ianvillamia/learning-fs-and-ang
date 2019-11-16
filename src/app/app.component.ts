81179ee39c5f42f03de39a19835a7673b639432f
ianvillamia <ianvillamia0123@gmail.com>
2019-11-16 21:32:49 +0800

    woop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,40 @@ import { Observable, from } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+items=[];
 constructor(private db:AngularFirestore){
 
 }
+ngOnInit(){
+  //snappshotCHanges will include id
+  this.items=[];
+  //reads from collection
+  this.db.collection('items',q=>q.orderBy('timestamp','desc')).snapshotChanges().subscribe(serverItems=>{
+    //data handles everytime data change in fs
+    serverItems.forEach(a=>{
+      a.payload.doc.data(); //json object representing inside a var
+      let item:any = a.payload.doc.data(); //any meaning to access all the data as well as id
+      item.id=a.payload.doc.id;
+      this.items.push(item);
+    });
+  });
+}
+
 add(){
   this.db.collection('items').add({
     timestamp:new Date(),
   });
+}
+update(item){
+  this.db.doc(`items/${item.id}`).update({
+    timestamp:new Date()
+
+  });
+
+}
+delete(item){
+  this.db.doc(`item/${item.id}`).delete();
+
 }
 
 
